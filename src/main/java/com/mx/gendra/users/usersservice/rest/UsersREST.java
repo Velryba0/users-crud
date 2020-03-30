626bd5ec86c10833ed812a61c82a31ed90acf7df
Fernando <fernando.sabaiz@gmail.com>
2020-03-30 00:30:55 -0600

    Config database

--- src/main/java/com/mx/gendra/users/usersservice/rest/UsersREST.java
+++ src/main/java/com/mx/gendra/users/usersservice/rest/UsersREST.java
@@ -1,15 +1,25 @@
 package com.mx.gendra.users.usersservice.rest;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+import com.mx.gendra.users.usersservice.entitys.User;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("users")
 public class UsersREST {
 
     @GetMapping
+    public ResponseEntity<List<User>> getUser() {
+        return null;
+    }
+
+    //@GetMapping
     public String hello() {
-        return "Hello Fucking World!!";
+        return "Hello World!!";
     }
 }
